d85bc1cae925dce479714a4aa87c4e4ff57cebec
MPauli04 <mariap.correav@upb.edu.co>
2026-02-18 13:22:47 -0500

    2. Condicionales Simples
    Ejercicios con condicionales simples

---
+++ 2. CondicionalesSimples/2. CondicionalesSimples/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _2.CondicionalesSimples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  string nombre = " ";
+              float sueldo = 0;
+              Console.WriteLine("Ingrese el nombre del usuario");
+              nombre = Console.ReadLine(); //Lo que llegue desde la consola, es tipo string
+
+              Console.WriteLine("Ingrese el sueldo del usuario");
+              sueldo = Convert.ToSingle(Console.ReadLine());
+
+              if (sueldo >= 3000)
+              {
+                  Console.WriteLine($"La persona {nombre}, debe abonar impuestos");
+              }*/
+
+            string nombre = " ";
+            int edad = 0;
+            Console.WriteLine("Ingrese el nombre del usuario");
+            nombre = Console.ReadLine(); //Lo que llegue desde la consola, es tipo string
+
+            Console.WriteLine("Ingrese su edad por favor");
+            edad = Convert.ToInt32(Console.ReadLine());
+
+            if (edad >= 18)
+            {
+                Console.WriteLine($"Le doy la bienvenida, {nombre}, a mi sitio web ");
+            }
+        }
+    }
+}
