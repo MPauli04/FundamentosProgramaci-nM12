8f2da2bbaa13ccedb1cdeb3e012530799de52072
MPauli04 <mariap.correav@upb.edu.co>
2026-02-27 10:48:07 -0500

    Tarea de condicionales compuestos
    Tarea de condicionales compuestos

---
+++ 4.CondicionalesDoblesEjercicio/4.CondicionalesDoblesEjercicio/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _4.CondicionalesDoblesEjercicio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pedir un número entero al usuario//
+
+            Console.Write("Ingresa un número entero: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            // Verificar si el número es positivo, negativo o cero
+            if (numero > 0)
+            {
+                Console.WriteLine("El número es positivo.");
+            }
+            else if (numero < 0)
+            {
+                Console.WriteLine("El número es negativo.");
+            }
+            else
+            {
+                Console.WriteLine("El número es cero.");
+            }
+        }
+    }
+}
