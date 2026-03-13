3652b0d7758615998df70b70ce5211ef88937755
MPauli04 <mariap.correav@upb.edu.co>
2026-03-13 13:28:38 -0500

    10.Ejercicio Ciclos
    10.Ejercicio Ciclos

---
+++ 10.EjercicioWhile/10.EjercicioWhile/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace _10.EjercicioWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int acumulador = 0;
+            int numero = 0; 
+            Console.WriteLine("Ingrese un numero entero positivo: ");
+            numero = int.Parse(Console.ReadLine());
+
+            while (numero >= 0)
+            { 
+                acumulador += numero;
+                Console.WriteLine("Ingrese un numero entero positivo: ");
+                numero = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine($"La suma de los numeros ingresados es: {acumulador}");
+        }
+    }
+}
