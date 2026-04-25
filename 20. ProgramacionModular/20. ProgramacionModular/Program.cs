8df44e356a457237b435fa1cdc92fcf3c1852e99
MPauli04 <mariap.correav@upb.edu.co>
2026-04-25 17:43:33 -0500

    20.ProgramacionModular
    20.ProgramacionModular

---
+++ 20. ProgramacionModular/20. ProgramacionModular/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+
+namespace _20.ProgramacionModular
+{
+    internal class Program
+    {
+        static int añoActual = 2026;
+        static void Main(string[] args)
+        {
+            //PROGRMACIÓN MODULAR
+            
+
+            string nombre = "Paulina";
+            string apellido = "Correa";
+            Console.WriteLine("Edad: " + EdadAñoNacimiento());
+            Saludo(nombre, apellido);
+            Saludo("Carlos", "Perez");
+            Console.WriteLine("Edad: " + EdadAñoNacimiento(2001));
+            Console.ReadLine();
+            BorrarPantalla();
+        }
+
+        //Procedimiento sin parámetros
+        static void BorrarPantalla()
+        {
+            Console.Clear();
+        }
+        //Procedimiento con parámetros
+        static void Saludo(string nombre, string apellido)
+        {
+            Console.WriteLine($"Bienvenid@ {nombre} {apellido} a la programación modular");
+        }
+
+        //Funciones sin parámetros
+        static int EdadAñoNacimiento()
+        {
+            int añoNacimineto = 1999;
+            int edad = añoActual - añoNacimineto;
+                return edad;    
+        }
+
+        //Funciones con parámetros
+        static int EdadAñoNacimiento(int añoNacimineto)
+        {     
+            return añoActual - añoNacimineto;
+        }
+    }
+}
