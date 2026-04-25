3f529a5e692c2b6ae35063cc86e2f87af9af4855
MPauli04 <mariap.correav@upb.edu.co>
2026-04-25 17:42:19 -0500

    18.EjercicioArreglos
    18.EjercicioArreglos

---
+++ 18. EjercicioArreglos/18. EjercicioArreglos/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _18.EjercicioArreglos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*18.Llenar un arreglo con 15 palabras ingresadas por teclado, luego invertir el contenido del arreglo
+               y mostrar por pantalla los elementos del arreglo original y los elementos del arreglo invertido.*/
+
+            string[] palabrasOriginal = new string[15];
+
+            Console.WriteLine("Por favor, ingresa 15 palabras:");
+
+            
+            for (int i = 0; i < palabrasOriginal.Length; i++)
+            {
+                Console.Write($"Palabra {i + 1}: ");
+                palabrasOriginal[i] = Console.ReadLine();
+            }
+
+            
+            string[] palabrasInvertidas = (string[])palabrasOriginal.Clone();
+
+            
+            Array.Reverse(palabrasInvertidas);
+
+            
+            Console.WriteLine("\n--- Arreglo Original ---");
+            Console.WriteLine(string.Join(", ", palabrasOriginal));
+
+            Console.WriteLine("\n--- Arreglo Invertido ---");
+            Console.WriteLine(string.Join(", ", palabrasInvertidas));
+
+        }
+    }
+}
---
+++ 18. EjercicioArreglos/18. EjercicioArreglos/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("18. EjercicioArreglos")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("18. EjercicioArreglos")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3ba34efe-a515-4b96-b6a3-09fc262f120e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
