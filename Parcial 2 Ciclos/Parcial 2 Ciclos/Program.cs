ca46c061885609736bc30939608db223a2352233
MPauli04 <mariap.correav@upb.edu.co>
2026-04-25 17:44:47 -0500

    Parcial 2 Ciclos
    Parcial 2 Ciclos

---
+++ Parcial 2 Ciclos/Parcial 2 Ciclos/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Parcial_2_Ciclos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Un profesor tiene las calificaciones de 18 alumnos y necesita procesar la información para obtener un análisis general del curso.Las notas se encuentran en un rango de 0 a 10.
+                 El programa debe realizar las siguientes operaciones:
+                             Leer por teclado la nota de cada uno de los 18 estudiantes.
+                 Calcular el promedio de todas las notas.
+                 Identificar y mostrar cuál fue la nota mayor y la nota menor entre las ingresadas.
+                 Contar cuántos estudiantes aprobaron la asignatura(nota mayor o igual a 6.0).
+                 Mostrar por pantalla:
+                             El promedio general.
+                 La nota más alta.
+                 La nota más baja.
+                 La cantidad de aprobados en el curso.*/
+
+            float calificacionIngresada = 0.0f;
+            float sumaDeNotas = 0.0f;
+            float notaMayor = 0.0f;
+            float notaMenor = 0.0f;
+            int aprobados = 0;
+            int alumnos = 18;
+            float promedioGeneral = 0.0f;
+
+            Console.WriteLine($"--- Procesando Notas de {alumnos} Alumnos ---");
+
+            for (int i = 1; i <= alumnos; i++)
+            {
+                Console.Write($"Ingrese la nota del alumno #{i}: ");
+                calificacionIngresada = float.Parse(Console.ReadLine());
+
+
+                sumaDeNotas += calificacionIngresada;
+
+                if (calificacionIngresada >= 6.0f)
+                {
+                    aprobados++;
+                }
+
+                if (calificacionIngresada > notaMayor)
+                {
+                    notaMayor = calificacionIngresada;
+                }
+
+                if (calificacionIngresada < 6.0f)
+                {
+                    notaMenor = calificacionIngresada;
+                }
+            }
+
+            promedioGeneral = sumaDeNotas / alumnos;
+
+            Console.WriteLine("RESULTADOS DEL CURSO");
+
+            Console.WriteLine($"Promedio general:{promedioGeneral}");
+            Console.WriteLine($"Nota más alta:{notaMayor}");
+            Console.WriteLine($"Nota más baja:{notaMenor}");
+            Console.WriteLine($"Total de aprobados:{aprobados}");
+
+        }
+    }
+}
---
+++ Parcial 2 Ciclos/Parcial 2 Ciclos/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Parcial 2 Ciclos")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Parcial 2 Ciclos")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fb39bff3-6445-4463-b0a5-9fc90e660f1d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
